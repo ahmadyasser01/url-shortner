3fcf366255a4fb0d19cea51129db5aa649b1485c
ahmadyasser01 <ahmadyasser2814@gmail.com>
2021-09-15 01:31:00 +0200

    added database

---
+++ src/models/links.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+
+const linksSchema = new mongoose.Schema({
+    slug: {
+        type: String,
+        required: false,
+        trim: true,
+        unique: true
+    },
+    url: {
+        type: String,
+        required: true,
+        trim: true,
+        validate(value) {
+            if (!validator.isURL(value)) {
+                throw new Error("not a url")
+            }
+        }
+
+    }
+})
+
+const Link = mongoose.model('Link', linksSchema)
+
+module.exports = Link;
