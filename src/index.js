46eeab5adb0a1fff18bbfedb2b609277a700a105
ahmadyasser01 <ahmadyasser2814@gmail.com>
2021-09-14 22:20:08 +0200

    basic app working

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path')
 
 
-
+app.use(express.json())
 const pathDir = path.join(__dirname, "../public")
 const port = process.env.PORT || 3000;
 
@@ -14,6 +14,21 @@ app.get('/', (req, res) => {
         message: "Hello-world, app works fine"
     })
 })
+
+const urls = {}
+app.post("/", (req, res) => {
+    const { slug, url } = req.body;
+    urls[slug] = url;
+    res.send(urls);
+
+})
+
+app.get('/:id', (req, res) => {
+    const slug = req.params.id;
+    const url = urls[slug]
+    res.redirect(url)
+})
+
 
 app.listen(port, () => {
     console.log(`app is listening on port ${port} ,  http://localhost:${port}`);
