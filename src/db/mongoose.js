430bc511344bc8762d99afe2fbc971848f5cfc77
ahmadyasser01 <ahmadyasser2814@gmail.com>
2021-09-15 14:03:52 +0200

    tesT

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
-
-try {
-    mongoose.connect(process.env.MONGODB_URL, {
+const connect = async () => {
+    await mongoose.connect(process.env.MONGODB_URL, {
         useNewUrlParser: true,
 
     }).then(connected => {
         console.log(connected)
     }).catch(err => { throw new Error("can;t connect") })
+}
+try {
+    connect();
 }
 catch (e) {
     return console.log(e);
