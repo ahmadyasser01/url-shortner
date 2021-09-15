5fad74c2e4803ee978c09a7d70f45d34646876b8
ahmadyasser01 <ahmadyasser2814@gmail.com>
2021-09-15 11:44:01 +0200

    finish app

--- public/js/client.js
+++ public/js/client.js
@@ -1,9 +1,42 @@
-const approot = document.getElementById('root')
+const approot = document.getElementById('app-root')
 // Select the template
 const test = document.querySelector('#test').innerHTML
 console.log(test)
-const html = Mustache.render(test, { message: "test" })
+let result = ''
+const html = Mustache.render(test, { result })
+approot.insertAdjacentHTML('afterend', html)
+
+const resultDiv = document.querySelector('.result');
+
+
+
 
 
 // Insert the template into the DOM
-approot.insertAdjacentHTML('afterend', html)
+
+const urlForm = document.querySelector('form');
+urlForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const urlInput = document.getElementById('floatingInput').value;
+    const slugInput = document.getElementById('floatingInput2').value;
+    console.log("urlis", urlInput, slugInput)
+    const response = await fetch('/', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: JSON.stringify({
+            url: urlInput,
+            slug: slugInput || undefined,
+        }),
+    });
+    if (response.ok) {
+        result = await response.json();
+        resultDiv.style.display = "block"
+        console.log('url is ', result.url)
+        resultDiv.innerHTML = `<a href=\' localhost:3000/ ${result.slug} \' > ${result.url}/${result.slug}</a > `;
+    }
+    else {
+        resultDiv.innerHTML = `Error :${response.status}`;
+    }
+})
